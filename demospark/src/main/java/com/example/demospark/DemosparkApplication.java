a8ad1344686e04df3d166e91e36eadef8eeb967b
yangx <yangxing@sunwayworld.com>
2020-12-22 16:57:24 +0800

    初始提交

---
+++ demospark/src/main/java/com/example/demospark/DemosparkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosparkApplication.class, args);
+	}
+
+}
---
+++ demospark/src/test/java/com/example/demospark/DemosparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
